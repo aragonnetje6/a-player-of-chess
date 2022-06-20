de672ce711915d8199ccb17971330763b50b9b89
aragonnetje6 <twanstok@gmail.com>
2022-06-20 23:19:37 +0200

    basic Game.get_available_moves implemented

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+#![feature(is_some_with)]
 extern crate core;
 
 use std::fmt::{Display, Formatter};
+
 use colored::Colorize;
 
 use Colour::*;
 use PieceClass::*;
+
 use crate::human_player::HumanPlayer;
 
 mod human_player;
@@ -13,6 +16,9 @@ fn main() {
     let player1 = HumanPlayer::new();
     let player2 = HumanPlayer::new();
     let mut game = Game::new(Box::new(player1), Box::new(player2));
+    let vec = game.get_available_moves(White);
+    println!("{:?}", vec);
+    println!("{}", vec.len());
     game.play();
 }
 
@@ -39,10 +45,10 @@ impl Game {
             for player in &self.players {
                 let player_move = player.get_move(&self);
                 if self.is_valid_move(&player_move, player.get_colour()) {
+                    self.execute_move(&player_move);
                     self.players
                         .iter()
                         .for_each(|x| x.move_made(&player_move, player.get_colour()));
-                    self.execute_move(player_move);
                 } else {
                     panic!("Bad move received, terminating game")
                 }
@@ -53,9 +59,282 @@ impl Game {
         false
     }
     fn is_valid_move(&self, player_move: &Move, colour: Colour) -> bool {
-        true
+        self.get_available_moves(colour).contains(player_move)
     }
-    fn execute_move(&self, player_move: Move) {}
+    fn execute_move(&self, _player_move: &Move) {
+        todo!();
+    }
+
+    fn get_available_moves(&self, colour: Colour) -> Vec<Move> {
+        let mut moves = vec![];
+        for (r, row) in self.board.iter().enumerate() {
+            for (c, piece_opt) in row.iter().enumerate() {
+                match piece_opt {
+                    None => {}
+                    Some(piece) => {
+                        if piece.owner != colour {
+                            continue;
+                        }
+                        match piece.class {
+                            Pawn(moved, _) => self.get_pawn_moves(colour, &mut moves, r, c, moved),
+                            Rook(_) => self.get_rook_moves(colour, &mut moves, r, c),
+                            Knight => self.get_knight_moves(colour, &mut moves, r, c),
+                            Bishop => self.get_bishop_moves(colour, &mut moves, r, c),
+                            Queen => self.get_queen_moves(colour, &mut moves, r, c),
+                            King(moved) => self.get_king_moves(colour, &mut moves, r, c, moved),
+                        }
+                    }
+                }
+            }
+        }
+        // todo: filter moves that result in check
+        return moves;
+    }
+
+    fn get_linear_moves(
+        &self,
+        colour: Colour,
+        moves: &mut Vec<Move>,
+        r: usize,
+        c: usize,
+        r_dir: isize,
+        c_dir: isize,
+    ) {
+        for i in 1..8 {
+            let terminated = self.get_stepwise_move(colour, moves, r, c, i * r_dir, i * c_dir);
+            if terminated {
+                break;
+            }
+        }
+    }
+
+    fn get_stepwise_move(
+        &self,
+        colour: Colour,
+        moves: &mut Vec<Move>,
+        r: usize,
+        c: usize,
+        r_dir: isize,
+        c_dir: isize,
+    ) -> bool {
+        let new_r: isize = r as isize + r_dir;
+        let new_c: isize = c as isize + c_dir;
+        if new_r < 0 || new_r >= 8 || new_c < 0 || new_c >= 8 {
+            return false;
+        }
+        match self.board[new_r as usize][new_c as usize] {
+            None => moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(new_r as usize, new_c as usize),
+            )),
+            Some(other) => {
+                if other.owner != colour {
+                    moves.push(Move::new(
+                        coords_to_string(r, c),
+                        coords_to_string(new_r as usize, new_c as usize),
+                    ))
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    fn get_king_moves(
+        &self,
+        colour: Colour,
+        moves: &mut Vec<Move>,
+        r: usize,
+        c: usize,
+        moved: bool,
+    ) {
+        // main moves
+        for (r_dir, c_dir) in [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (1, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+        ] {
+            self.get_stepwise_move(colour, moves, r, c, r_dir, c_dir);
+        }
+        // Castle left
+        if !moved
+            && self.board[r][0].is_some_and(|p| *p == Piece::new(colour, Rook(true)))
+            && (1..c).all(|i| self.board[r][i].is_none())
+        {
+            moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(r, c - 2),
+            ))
+        }
+        // Castle right
+        if !moved
+            && self.board[r][7].is_some_and(|p| *p == Piece::new(colour, Rook(true)))
+            && (c + 1..7).all(|i| self.board[r][i].is_none())
+        {
+            moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(r, c + 2),
+            ))
+        }
+    }
+
+    fn get_knight_moves(&self, colour: Colour, moves: &mut Vec<Move>, r: usize, c: usize) {
+        for (r_dir, c_dir) in [
+            (-2, -1),
+            (-2, 1),
+            (2, -1),
+            (2, 1),
+            (-1, -2),
+            (-1, 2),
+            (1, -2),
+            (1, 2),
+        ] {
+            self.get_stepwise_move(colour, moves, r, c, r_dir, c_dir);
+        }
+    }
+    fn get_queen_moves(&self, colour: Colour, moves: &mut Vec<Move>, r: usize, c: usize) {
+        for (r_dir, c_dir) in [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (1, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+        ] {
+            self.get_linear_moves(colour, moves, r, c, r_dir, c_dir);
+        }
+    }
+
+    fn get_bishop_moves(&self, colour: Colour, moves: &mut Vec<Move>, r: usize, c: usize) {
+        for (r_dir, c_dir) in [(1, 1), (-1, -1), (-1, 1), (1, -1)] {
+            self.get_linear_moves(colour, moves, r, c, r_dir, c_dir);
+        }
+    }
+
+    fn get_rook_moves(&self, colour: Colour, moves: &mut Vec<Move>, r: usize, c: usize) {
+        for (r_dir, c_dir) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            self.get_linear_moves(colour, moves, r, c, r_dir, c_dir);
+        }
+    }
+
+    fn get_pawn_moves(
+        &self,
+        colour: Colour,
+        moves: &mut Vec<Move>,
+        r: usize,
+        c: usize,
+        moved: bool,
+    ) {
+        let dir: isize = match colour {
+            Black => 1,
+            White => -1,
+        };
+        let new_r = r as isize + dir;
+        let new_r_2 = r as isize + dir * 2;
+        if new_r < 0 || new_r >= 8 {
+            return;
+        }
+        if self.board[new_r as usize][c].is_none() {
+            if new_r == 0 && new_r == 7 {
+                for class in [Queen, Knight, Rook(true), Bishop] {
+                    moves.push(Move::promotion(
+                        coords_to_string(r, c),
+                        coords_to_string(new_r as usize, c),
+                        class,
+                    ))
+                }
+            } else {
+                moves.push(Move::new(
+                    coords_to_string(r, c),
+                    coords_to_string(new_r as usize, c),
+                ));
+            }
+        }
+        if !moved && self.board[new_r_2 as usize][c].is_none() {
+            moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(new_r_2 as usize, c),
+            ));
+        }
+        if c + 1 < 8 && self.board[new_r as usize][c + 1].is_some_and(|other| other.owner != colour)
+        {
+            if new_r == 0 && new_r == 7 {
+                for class in [Queen, Knight, Rook(true), Bishop] {
+                    moves.push(Move::promotion(
+                        coords_to_string(r, c),
+                        coords_to_string(new_r as usize, c + 1),
+                        class,
+                    ))
+                }
+            } else {
+                moves.push(Move::new(
+                    coords_to_string(r, c),
+                    coords_to_string(new_r as usize, c + 1),
+                ));
+            }
+        }
+        if c.checked_sub(1).is_some()
+            && self.board[new_r as usize][c - 1].is_some_and(|other| other.owner != colour)
+        {
+            if new_r == 0 && new_r == 7 {
+                for class in [Queen, Knight, Rook(true), Bishop] {
+                    moves.push(Move::promotion(
+                        coords_to_string(r, c),
+                        coords_to_string(new_r as usize, c - 1),
+                        class,
+                    ))
+                }
+            } else {
+                moves.push(Move::new(
+                    coords_to_string(r, c),
+                    coords_to_string(new_r as usize, c - 1),
+                ));
+            }
+        }
+        if c.checked_sub(1).is_some()
+            && self.board[r][c - 1]
+                .is_some_and(|p| p.owner != colour && p.class == Pawn(true, true))
+        {
+            moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(new_r as usize, c - 1),
+            ));
+        }
+        if c + 1 < 8
+            && self.board[r][c + 1]
+                .is_some_and(|p| p.owner != colour && p.class == Pawn(true, true))
+        {
+            moves.push(Move::new(
+                coords_to_string(r, c),
+                coords_to_string(new_r as usize, c + 1),
+            ));
+        }
+    }
+}
+
+fn coords_to_string(row: usize, col: usize) -> String {
+    format!(
+        "{}{}",
+        match col {
+            0 => "a",
+            1 => "b",
+            2 => "c",
+            3 => "d",
+            4 => "e",
+            5 => "f",
+            6 => "g",
+            7 => "h",
+            _ => panic!("Invalid coordinate"),
+        },
+        row.to_string()
+    )
 }
 
 fn new_board() -> Board {
@@ -74,10 +353,10 @@ fn new_board() -> Board {
         board[0][i] = Some(Piece::new(Black, *class));
     }
     for i in 0..8 {
-        board[1][i] = Some(Piece::new(Black, Pawn));
+        board[1][i] = Some(Piece::new(Black, Pawn(false, false)));
     }
     for i in 0..8 {
-        board[6][i] = Some(Piece::new(White, Pawn));
+        board[6][i] = Some(Piece::new(White, Pawn(false, false)));
     }
     for (i, class) in piece_classes.iter().enumerate() {
         board[7][i] = Some(Piece::new(White, *class));
@@ -85,7 +364,7 @@ fn new_board() -> Board {
     return board;
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 struct Piece {
     owner: Colour,
     class: PieceClass,
@@ -103,7 +382,7 @@ impl Display for Piece {
             f,
             "{}",
             match self.class {
-                Pawn => match self.owner {
+                Pawn(_, _) => match self.owner {
                     Black => "P".black(),
                     White => "P".white(),
                 },
@@ -132,9 +411,9 @@ impl Display for Piece {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum PieceClass {
-    Pawn,
+    Pawn(bool, bool),
     Rook(bool),
     Knight,
     Bishop,
@@ -142,7 +421,7 @@ enum PieceClass {
     King(bool),
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum Colour {
     Black,
     White,
@@ -155,9 +434,11 @@ trait Player {
     fn move_made(&self, opponent_move: &Move, colour: Colour);
 }
 
+#[derive(Clone, PartialEq, Debug)]
 struct Move {
     src: String,
     dst: String,
+    promotion: Option<PieceClass>,
 }
 
 impl Display for Move {
@@ -168,6 +449,17 @@ impl Display for Move {
 
 impl Move {
     fn new(src: String, dst: String) -> Move {
-        Move { src, dst }
+        Self {
+            src,
+            dst,
+            promotion: None,
+        }
+    }
+    fn promotion(src: String, dst: String, promote_to: PieceClass) -> Move {
+        Self {
+            src,
+            dst,
+            promotion: Some(promote_to),
+        }
     }
 }
